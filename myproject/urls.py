5f7adea72dae66475ec6c359d0e46dd33272f367
dzouelouiam <dzouelouiammohamed@gmail.com>
2023-05-22 12:55:03 +0100

    api

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
-
     
 urlpatterns = [
     path("admin/", admin.site.urls),
     #routing the user to go to the pages to match the root ( urls.py base)
-    path('',include('base.urls'))
+    path('',include('base.urls')),
+    path('api/',include('base.api.urls'))
 ]
